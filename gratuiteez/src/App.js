fe5665cf595aeb08d932c764650245ebff75cc95
cori-peck <cori.peck@ymail.com>
2019-02-13 15:05:28 -0600

    project stop

--- gratuiteez/src/App.js
+++ gratuiteez/src/App.js
@@ -1,27 +1,20 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
-
-import HomePage from './HomePage/HomePage';
-import LoginPage from './LoginPage/LoginPage';
-import RegisterPage from './RegisterPage/RegisterPage.employee';
+import TippeeList from './component/TippeeList';
+import HomePage from './component/HomePage';
+import { Route, withRouter} from 'react-router-dom';
 
 class App extends Component {
  
   render() {
     return (
       <div className="App">
-        <div className="container">
-              <Router>
-                  <Route exact path="/:filter?" component={HomePage} />
-                  <Route path="/login" component={LoginPage} />
-                  <Route path="/register" component={RegisterPage} />
-              </Router>
-          </div>
+      <Route exact path="/" component={HomePage} />
+       <Route path="/tippees" component={TippeeList} />
         </div>
     );
   }
 }
 
 
-export default App
+export default withRouter(App)
--- gratuiteez/src/index.js
+++ gratuiteez/src/index.js
@@ -1,17 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { Provider } from 'react-redux';
-
 import App from './App';
-import { store } from './helpers/store';
-
+import { BrowserRouter as Router } from "react-router-dom";
 
 
 ReactDOM.render(
-    <Provider store={store}>
-        <App />
-    </Provider>, 
-    document.getElementById('root')
-);
-
+<Router>
+  <App />
+</Router>,
+document.getElementById('root'));
